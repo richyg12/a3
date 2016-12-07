28503ae6705638137738a100f879c78c1a555acd
richyg12 <rgazey@gmail.com>
2016-12-07 22:14:07 +0000

    commited to start a3

---
+++ proj/src/main/java/gazey/App.java
@@ -0,0 +1,13 @@
+package gazey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
